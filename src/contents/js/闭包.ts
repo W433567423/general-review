0b5855548771fe780eb591804fa0d10184888149
tutu <t433567423@163.com>
2023-11-07 13:29:10 +0800

    feat: JS-闭包

--- src/contents/js/闭包.ts
+++ src/contents/js/闭包.ts
@@ -6,3 +6,15 @@
  * @author: tutu
  * @time: 2023-11-07 13:18
  */
+function  main(){
+    var total= 1000
+    function pay(money:number){
+        total-=money
+        console.log(`花了${money}钱,剩余${total}钱`)
+    }
+    pay(100)
+    pay(50)
+}
+main()
+
+// 使用全局变量会不安全（易被污染），使用局部变量无法保存状态，故使用闭包来解决
